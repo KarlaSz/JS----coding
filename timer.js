e54d6c7e19112b5c8f42dccd627270233867c2b1
KarlaSz <szymaszkiewiczk@gmail.com>
2023-04-24 11:46:04 +0200

    Create timer.js

---
+++ timer.js
@@ -0,0 +1,21 @@
+function odliczanie()
+        {
+            var dzisiaj = new Date();
+
+            var dzien = dzisiaj.getDate();
+            var miesiac = dzisiaj.getMonth()+1;
+            var rok = dzisiaj.getFullYear();
+
+            var godzina = dzisiaj.getHours();
+            if(godzina<10) godzina = "0"+godzina;
+
+            var minuta = dzisiaj.getMinutes();
+            if(godzina<10) minuta = "0"+godzina;
+
+            var sekunda = dzisiaj.getSeconds();
+            if(godzina<10) sekundy = "0"+godzina;
+
+            document.getElementById("zegar").innerHTML = dzien+"/"+miesiac+"/"+rok+" | "+godzina+":"+minuta+":"+sekunda;
+        
+        setTimeout("odliczanie()",1000);
+        }
