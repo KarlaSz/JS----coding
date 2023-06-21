b99218bc4191d8c331aa509c6427948972a7c88c
KarlaSz <szymaszkiewiczk@gmail.com>
2023-06-21 14:28:36 +0200

    Update script.js

--- Js-coursera/script.js
+++ Js-coursera/script.js
@@ -63,3 +63,123 @@
 // petla zaczyna sie od 0 i 0 jest mniejsze od 10 i zwiekszane
 // jest za kazdy razem o 1, wiec zaczyna od 0 ,a konczy na 9
 //bo 10 nie jest mniejsze od 10
+
+//-----> Tworzenie obiektu New syntax
+
+// let company = new Object; 
+// company.name = "Facebook";
+// company.ceo = new Object; 
+// company.ceo.fistName = "Mark";
+
+// console.log(company);
+// console.log("Company CEO name is: " + company.ceo.fistName);
+// console.log(company.ceo["fistName"]);
+// console.log(company["name"]);
+
+// let stockPropName = "stock of company"; company[stockPropName] = 110;
+// console.log("Stock price is: " + company[stockPropName]);
+
+//-----> Better way: object literal
+//obiekt literal zawiera elemney w sobie i rozdzielone przecinkiem,a nie ;
+//jest to szybsze i latwiejsze niz klasyczny object new syntax
+
+
+// let facebook = {
+//     name: "Facebook",
+//     ceo: {
+//         firstName: "Mark",
+//         favColor: "blue",
+//         surname: "Zuck"
+//     },
+//     "stock of company": 110
+// };
+
+// console.log(facebook);
+// console.log(facebook.ceo.surname);
+
+
+
+//------------------
+
+//-----> Functions
+// funckja sa obiektami ze specjalnymi wlasciwosciami 
+// and first-class data types
+
+// function multiply (x,y) {
+//     return x * y;
+// }
+
+// console.log(multiply(5,3));
+
+// multiply.version = "v.1.0.";
+// console.log(multiply);
+// console.log(multiply.toString());
+// console.log(multiply.version);
+
+// Function factory 
+
+// function makeMultiplier(multiplier) {
+//     var myFunc = function (x) {
+//         return multiplier * x;
+//     };
+
+//     return myFunc;
+// }
+
+// var multiplyBy3 = makeMultiplier(3);
+// console.log(multiplyBy3(10));
+// var doubleAll = makeMultiplier(2);
+// console.log(doubleAll(100));
+
+// //Passing functions as arguments 
+
+// function doOperationOn(x, operation) {
+//     return operation(x);
+// }
+
+// var result = doOperationOn(5, multiplyBy3);
+// console.log(result);
+
+// result = doOperationOn(100, doubleAll);
+// console.log(result);
+
+//-----> Arrays - tablice
+
+// var array = new Array();
+// array[0] = "Karo";
+// array[1] = 2;
+// array[2] = function (name) {
+//     console.log("hello " + name );
+// };
+// array[3] = {Kurs: "html, css, js"};
+
+// console.log(array);
+// console.log(array[1]);
+// array[2](array[0]);
+
+//-----> Shorthand array
+
+// var names = ["Karo", "Ela", "Figi"];
+// console.log(names);
+
+
+// for (var i = 0; i < names.length; i++) {
+//     console.log("hello " + names[i]);
+// }
+
+// var names2 = ["Karo", "Ela", "Figi"];
+
+// var myObj = {
+//     name: "Karo",
+//     courser: "html,css,js",
+//     platform: "coursera"
+// };
+
+// for (var prop in myObj) {
+//     console.log(prop + ": " + myObj[prop])
+// }
+
+//-----> 
+
+
+
