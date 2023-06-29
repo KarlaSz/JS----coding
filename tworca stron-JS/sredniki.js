9c2686380e4193ac80e5496fc0a2c6c80c864da6
KarlaSz <szymaszkiewiczk@gmail.com>
2023-06-29 14:22:31 +0200

    js-trening

---
+++ tworca stron-JS/sredniki.js
@@ -0,0 +1,17 @@
+//sredniki w JS, sa po to zeby rozdzielac instrukcje 
+
+// const a = 2;
+// const b = 3;
+
+// function add (x, y) {
+//     return x + y;
+// }
+
+// add(2, 3);
+
+//PETLE - for, i od iteracji powtorzen, nie moze byc const
+//bo zmienna zmienia sie i zwieksza
+
+for (let i=0, i < 10; i++ ) {
+    console.log(i)
+}
