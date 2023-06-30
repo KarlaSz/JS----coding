1c922fa9965112286503669ece99fcf697195ea6
KarlaSz <szymaszkiewiczk@gmail.com>
2023-06-30 14:03:43 +0200

    Create operatory-jednoArgumentowe.js

---
+++ tworca stron-JS/operatory-jednoArgumentowe.js
@@ -0,0 +1,19 @@
+// +'4' - operator + zmienia stringa na liczbe
+
+// +
+
+value.valueOf()
+.value.toString()
+
+7 + +'4' // dodawanie,a druga wartosc string jest konwertowana na number
+
+
+!false
+!true
+
+!!'hello'
+true
+
+//wartosc prawdziwa to boolean('[]') = true
+
+//podwojna negacja robi to samo co boolean,ale zapis jest krotszy
