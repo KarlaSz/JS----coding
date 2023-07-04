e4e9db5759f17479032e84a56974370ce9e2c979
KarlaSz <szymaszkiewiczk@gmail.com>
2023-07-04 09:42:41 +0200

    Create import.js

---
+++ tworca stron-JS/import.js
@@ -0,0 +1,12 @@
+import User, { user, sayHi } from './user-import.js'
+// import User from './user-import.js'
+
+//mozna tez exportowac wszystkie zmienne i dane zamiast
+//wypisac po kolei w klamrach 
+// import User, * as userModule from './user-import.js'
+
+// console.log(userModule)
+console.log(user)
+console.log(User)
+sayHi()
+
